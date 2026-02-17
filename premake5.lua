70c3e65b2d9aa247ba24003a84ecc575951126dc
Masterride <43613854+Masterride05@users.noreply.github.com>
2026-02-17 22:31:40 +0100

    Added premake5 script

---
+++ premake5.lua
@@ -0,0 +1,22 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "*.cpp",
+        "*.h",
+        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_opengl3.cpp"
+    }
+
+    includedirs {
+        "./",
+        "../glfw/include"
+    }
+    
+    filter "system:windows"
+        staticruntime "on"
